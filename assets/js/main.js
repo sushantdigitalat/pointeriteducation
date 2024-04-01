bcae432bcd3d985f0e02e19424cbae2c21ef6ebb
sushantdigitalat <sushant.khairnar@digitalat.in>
2024-04-01 13:31:26 +0530

    010424

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function () {
-  "use strict";
+  ("use strict");
 
   /**
    * Easy selector helper function
@@ -178,89 +178,65 @@
   /**
    * Testimonials slider
    */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
+  // new Swiper(".testimonials-slider", {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 40,
+  //     },
 
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-    },
-  });
+  //     1200: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 40,
+  //     },
+  //   },
+  // });
 
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-      });
+  // window.addEventListener("load", () => {
+  //   let portfolioContainer = select(".portfolio-container");
+  //   if (portfolioContainer) {
+  //     let portfolioIsotope = new Isotope(portfolioContainer, {
+  //       itemSelector: ".portfolio-item",
+  //     });
 
-      let portfolioFilters = select("#portfolio-flters li", true);
+  //     let portfolioFilters = select("#portfolio-flters li", true);
 
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
+  //     on(
+  //       "click",
+  //       "#portfolio-flters li",
+  //       function (e) {
+  //         e.preventDefault();
+  //         portfolioFilters.forEach(function (el) {
+  //           el.classList.remove("filter-active");
+  //         });
+  //         this.classList.add("filter-active");
 
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
+  //         portfolioIsotope.arrange({
+  //           filter: this.getAttribute("data-filter"),
+  //         });
+  //         portfolioIsotope.on("arrangeComplete", function () {
+  //           AOS.refresh();
+  //         });
+  //       },
+  //       true
+  //     );
+  //   }
+  // });
 
   /**
    * Animation on scroll
@@ -298,3 +274,16 @@ window.onscroll = function (e) {
     left.className = "";
   }
 };
+
+/** Modal Popup **/
+
+// When the user clicks on div, open the popup
+function openPopup() {
+  var popup = document.getElementById("popupOverlay");
+  popup.classList.toggle("show");
+}
+
+function closePopup() {
+  var popup = document.getElementById("popupOverlay");
+  popup.classList.toggle("show");
+}
